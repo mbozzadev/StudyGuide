618037e82ec7c8214448faac18346075e468bd53
mbozzadev <michaelbbozzadev@gmail.com>
2025-08-14 20:34:24 -0400

    Added form, buttons, dialogs to find file

---
+++ StudyCfg.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyGuide
+{
+    public struct StudyCfg
+    {
+        public string CONFIG;
+        public string rootLocation;
+    }
+}
